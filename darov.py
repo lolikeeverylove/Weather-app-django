90007806e40dcb419ada56a6293b82fdf588e89c
lolike <girev.alex@yandex.ru>
2019-11-22 02:25:00 +0300

    fdsfd

---
+++ darov.py
@@ -0,0 +1,5 @@
+class hii:
+    def __init__(self, lel):#это типо конструктор
+        self.lel = lel
+    def kek(self):
+        print(self.lel)
---
+++ kek.py
@@ -0,0 +1,3 @@
+import darov
+darov = darov.hii(5)
+print(darov.kek())
